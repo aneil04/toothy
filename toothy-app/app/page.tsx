aa6cb9cd2a63f113aa4bed4390006548c8f264c0
aneil04 <aneil2642@gmail.com>
2025-09-28 02:55:50 -0400

    refactor

--- toothy-app/app/page.tsx
+++ toothy-app/app/page.tsx
@@ -1,146 +1,121 @@
-'use client';
+"use client"
 
-import ThreeScene from './components/ThreeScene';
-import { useEffect, useRef, useState } from "react";
+import { useState, useEffect, useRef } from "react"
+import { Button } from "@/components/ui/button"
+import ThreeScene from "./components/ThreeScene"
+import WebcamCapture from "./components/WebcamCapture"
 
-export default function Home() {
-  // Example: You can pass a model URL to load a 3D model
-  // const modelUrl = '/path/to/your/model.glb';
-
-  return (
-    <div className="w-full h-screen bg-red-300 p-10">
-      {/* To load a model, uncomment and modify the line below: */}
-      {/* <WebcamCapture /> */}
-    </div>
-  );
+const name2material_index = {
+  "closed_left": 0,
+  "closed_mid": 1,
+  "closed_right": 2,
+  "open_left_down": 4,
+  "open_mid_down": 6,
+  "open_right_down": 8,
+  "open_left_up": 5,
+  "open_mid_up": 7,
+  "open_right_up": 9,
 }
 
-export function WebcamCapture({ setInferenceResult }: { setInferenceResult: (result: any) => void }) {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [imgUrl, setImgUrl] = useState<string | null>(null);
-  const sendingRef = useRef(false);
-  const rafRef = useRef<number | null>(null);
+export default function VideoStreamPage() {
+  const [isRunning, setIsRunning] = useState(false)
+  const [time, setTime] = useState(0)
+  const [inferenceResult, setInferenceResult] = useState<any>(null)
+  const [materials, setMaterials] = useState<number[]>([1, 1, 1, 0, 1, 1, 1, 1, 1, 1])
 
   useEffect(() => {
-    const enableWebcam = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: { width: 640, height: 480 }, // you can adjust resolution
-          audio: false, // disable audio if not needed
-        });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-          await videoRef.current.play();
-        }
-      } catch (err) {
-        console.error("Error accessing webcam:", err);
-      }
-    };
+    let interval: NodeJS.Timeout | null = null
 
-    enableWebcam();
-  }, []);
-
-  // Capture frames on an interval
-  useEffect(() => {
-    const offscreenCanvas = document.createElement("canvas");
-    let frameCount = Infinity
-
-    const loop = async () => {
-      if (!videoRef.current || !offscreenCanvas) {
-        rafRef.current = requestAnimationFrame(loop);
-        return;
-      }
-
-      if (sendingRef.current) {
-        rafRef.current = requestAnimationFrame(loop);
-        return;
-      }
-
-      sendingRef.current = true;
-      frameCount++;
-      try {
-        const ctx = offscreenCanvas.getContext("2d");
-        if (!ctx) return;
-
-        offscreenCanvas.width = videoRef.current.videoWidth;
-        offscreenCanvas.height = videoRef.current.videoHeight;
-        ctx.drawImage(videoRef.current, 0, 0, offscreenCanvas.width, offscreenCanvas.height);
-
-        const frameBlob = await new Promise<Blob>((resolve) =>
-          (offscreenCanvas).toBlob(
-            (b) => resolve(b!),
-            "image/jpeg",
-            0.7
-          )
-        );
-
-        if (!frameBlob) {
-          sendingRef.current = false;
-        } else {
-          // send blob to backend
-          const form = new FormData();
-          form.append("image", frameBlob, "frame.jpg");
-
-          if (frameCount > 10) {
-            frameCount = 0
-            console.log("inference...");
-            fetch("http://localhost:8000/infer", { method: "POST", body: form }).then(async (res) => {
-              if (res.ok) {
-                // Expect JSON from FastAPI: { detections: [...], latency_ms: ... }
-                const json = await res.json();
-                console.log("Inference response:", json);
-
-                if (Object.keys(json).length > 0) {
-                  setInferenceResult(json);
-                }
-              } else {
-                console.warn("Inference HTTP error:", res.status);
-              }
-            });
-          }
-
-          const r = await fetch("http://localhost:8000/detect", { method: "POST", body: form });
-          if (r.ok) {
-            // Expect JSON from FastAPI: { detections: [...], latency_ms: ... }
-            const blob = await r.blob();
-            // Clean last URL to avoid memory leak
-            setImgUrl((prev) => {
-              if (prev) URL.revokeObjectURL(prev);
-              return URL.createObjectURL(blob);
-            });
-            sendingRef.current = false;
-          } else {
-            console.warn("Inference HTTP error:", r.status);
-          }
-        }
-      } catch (err) {
-        console.error("Error capturing frame:", err);
-      } finally {
-        sendingRef.current = false;
-      }
-
-      rafRef.current = requestAnimationFrame(loop);
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1)
+      }, 1000)
+    } else if (!isRunning && time !== 0) {
+      if (interval) clearInterval(interval)
     }
 
-    rafRef.current = requestAnimationFrame(loop);
-
     return () => {
-      if (rafRef.current) {
-        cancelAnimationFrame(rafRef.current);
-      }
-    };
-  }, []);
+      if (interval) clearInterval(interval)
+    }
+  }, [isRunning, time])
+
+  const incrementMaterial = (index: number) => {
+    setMaterials((prevMaterials) => {
+      const newMaterials = [...prevMaterials]
+      newMaterials[index] = Math.max((newMaterials[index] + 1), 3)
+      return newMaterials
+    })
+  }
+  
+  useEffect(() => {
+    if (!inferenceResult) {
+      return
+    }
+
+    const section = inferenceResult.pred_name
+    const index = name2material_index[section as keyof typeof name2material_index]
+    incrementMaterial(index)
+  }, [inferenceResult])
+
+  const formatTime = (seconds: number) => {
+    const mins = Math.floor(seconds / 60)
+    const secs = seconds % 60
+    return `${mins}:${secs.toString().padStart(2, "0")}`
+  }
+
+  const handleStartStop = () => {
+    setIsRunning(!isRunning)
+  }
+
+  const handleReset = () => {
+    setTime(0)
+    setIsRunning(false)
+  }
 
   return (
-    <div>
-      <video
-        ref={videoRef}
-        autoPlay
-        muted
-        playsInline
-        className='hidden'
-      />
-      {imgUrl && <img src={imgUrl} alt="Processed" className="rounded-3xl shadow-lg" />}
+    <div className="flex flex-row gap-20 justify-center bg-gray-200 h-screen p-20">
+      {/* Video Stream Area - Left Side */}
+      <div className="flex flex-col gap-8">
+        <WebcamCapture setInferenceResult={setInferenceResult} />
+        <div className="flex flex-col gap-2">
+          <span className="text-4xl text-gray-600 font-light">{inferenceResult ? `Prediction: ${inferenceResult.pred_name}` : "inference..."}</span>
+        </div>
+      </div>
+
+      {/* Mobile Device Mockup - Right Side */}
+      <div className="flex relative items-center bg-red-500 aspect-[9/18] rounded-[3rem] shadow-lg">
+        {/* Phone notch */}
+        <div className="absolute top-6 left-1/2 transform -translate-x-1/2 bg-black rounded-full w-32 h-6 z-10"></div>
+
+        {/* Phone screen with full canvas */}
+        <div className="bg-white rounded-[2.5rem] border-8 border-black w-full h-full relative overflow-hidden">
+          {/* <canvas ref={canvasRef} className="w-full h-full bg-red-200" width={320} height={640} /> */}
+          <ThreeScene modelUrl="/teeth.glb" materials={materials} />
+          <div className="absolute top-20 left-0 right-0 text-center z-20">
+            <div className="text-6xl text-black rounded-lg mx-6 py-2">
+              {formatTime(time)}
+            </div>
+          </div>
+
+          <div className="absolute bottom-20 left-0 right-0 flex gap-3 justify-center items-center z-20">
+            <Button
+              onClick={handleStartStop}
+              className="bg-blue-500 hover:bg-blue-600 border border-blue-500 hover:border-blue-600 text-white px-20 py-6 rounded-xl text-lg backdrop-blur-sm shadow-lg"
+            >
+              {isRunning ? "stop" : "start"}
+            </Button>
+            {time > 0 && (
+              <Button
+                onClick={handleReset}
+                variant="outline"
+                className="px-20 py-6 rounded-xl text-lg bg-white/90 backdrop-blur-sm shadow-lg"
+              >
+                reset
+              </Button>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
